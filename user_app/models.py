ab27d6bb3e29a6176e45c05242ff6f6d3e29a30e
Hamid Axtamov <hamidaxtamov1@gmail.com>
2023-12-16 09:54:28 +0500

    x

---
+++ user_app/models.py
@@ -0,0 +1,77 @@
+import os
+
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager as DjangoBaseUserManager
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseUserManager(DjangoBaseUserManager):
+    """
+    Define a model manager for User model with no username field.
+    """
+    use_in_migrations = True
+
+    def _create_user(self, email, password, birth_date, gender, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, birth_date, gender, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, birth_date, gender, **extra_fields)
+
+    def create_superuser(self, email, password, birth_date, gender, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, birth_date, gender, **extra_fields)
+
+
+class CustomUserModel(AbstractUser):
+    """User model."""
+
+    def user_directory_path(instance, filename):
+        return f'users/{instance.email}/profile/{filename}'
+
+    username = None
+
+    email = models.EmailField(_("email address"), unique=True, help_text=_("Required. Your email address."))
+
+    birth_date = models.DateField(default=timezone.now, help_text=_("Required. Your birth date."))
+
+    CHOICES = (('M', 'Male'), ('F', 'Female'))
+    gender = models.CharField(max_length=1, choices=CHOICES, default='M', help_text=_("Required. Choose your gender."))
+
+    phone_number = models.CharField(max_length=13, blank=True, null=True, help_text=_('Enter phone number'
+                                                                                      ' e.g: +998123456789'))
+
+    profile_img = models.ImageField(upload_to=user_directory_path, null=True, blank=True,
+                                    help_text=_('Your profile picture.'))
+
+    objects = BaseUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['birth_date', 'gender']
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = "user"
+        verbose_name_plural = "users"
