187d46cbe70723d6a463bcc9846a3655a832adc3
Hamid Axtamov <hamidaxtamov1@gmail.com>
2023-12-18 18:43:35 +0500

    debug&crispy add

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('contest_app.urls')),
-    path('user/', include('user_app.urls'))
+    path('user/', include('user_app.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
