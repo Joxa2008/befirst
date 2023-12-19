d179b2a02a5477998d16b72b6d310c9c1f3241f5
Hamid Axtamov <hamidaxtamov1@gmail.com>
2023-12-20 00:12:58 +0500

    message animation

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -1,20 +1,41 @@
 function navDropDown() {
-  document.getElementById("navdropdown").classList.toggle("show");
+    document.getElementById("navdropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
 }
+
+let submitButton = document.getElementById('search-submit');
+let searchInput = document.getElementById('nav-search');
+
+searchInput.addEventListener('input', function () {
+    let valueInput = searchInput.value;
+    if (valueInput.trim() !== '') {
+        submitButton.style.visibility = 'visible';
+        submitButton.style.width = '100px';
+        submitButton.innerText = 'Search'
+        submitButton.style.padding = '3px 5px';
+    } else {
+        submitButton.style.visibility = 'hidden';
+        submitButton.style.padding = '0';
+        submitButton.style.width = '0px';
+        submitButton.innerText = ''
+
+    }
+
+});
+
 
 
 
