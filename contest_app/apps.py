cc41449d3d981dd4f41688e33bfe7f9ee01785b7
Hamid Axtamov <hamidaxtamov1@gmail.com>
2023-12-21 10:54:37 +0500

    start expersts\work

--- contest_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContestAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contest_app'
-
-    def ready(self):
-        import contest_app.signals
